6a96912c547ab68f0116e90e6878f2acc4323bb5
barry <nicolai.bernhard03@gmail.com>
2022-02-02 15:54:14 +0100

    Fixed static paths

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,37 +1,39 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { useRouter } from "next/router";
-import { Character } from "../types";
+import { Character, getCharacterResults } from "../types";
 
-const Character: NextPage<{ character: any }> = ({ character }) => {
-  const router = useRouter();
-  const { id } = router.query;
+const Character: NextPage<{ character: Character }> = ({ character }) => {
   return (
     <div>
-      <h1>Hello {character}</h1>
+      <h1>Hello {character.name}</h1>
     </div>
   );
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const res = await fetch("https://rickandmortyapi.com/api/character");
-  const { results } = await res.json();
+  const { results }: getCharacterResults = await res.json();
 
   const paths = results.map((result: Character) => ({
-    params: { id: result.id },
+    params: { id: result.id.toString() },
   }));
 
   return { paths, fallback: false };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const router = useRouter();
-  const { id } = router.query;
+  console.log(params);
+
   const res = await fetch(
-    `https://rickandmortyapi.com/api/character/${params.id}`
+    `https://rickandmortyapi.com/api/character/${params?.id}`
   );
-  const { results } = await res.json();
+  console.log("RES" + (await res));
+
+  const results = await res.json();
+  console.log("RESULTS" + (await results));
+
   return {
-    props: { character: { res } },
+    props: { character: results },
   };
 };
 
